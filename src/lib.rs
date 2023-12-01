281e8ff4b9d479f6b89bd5d01e3754f72e0ffbac
Akiomi Kamakura <akiomik@gmail.com>
2023-12-02 03:23:44 +0900

    Add doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 use std::io::prelude::*;
 
+/// Reads mecab-ipadic-neologd as vibrato::Dictionary
+///
+/// # Examples
+///
+/// ```
+/// let dict = vibrato_dict_ipa_neologd::read_dict().unwrap();
+/// let tokenizer = vibrato::Tokenizer::new(dict);
+/// let mut worker = tokenizer.new_worker();
+/// worker.reset_sentence("本とカレーの街神保町へようこそ。");
+/// worker.tokenize();
+///
+/// assert_eq!(9, worker.num_tokens());
+/// assert_eq!("本", worker.token(0).surface());
+/// assert_eq!("と", worker.token(1).surface());
+/// assert_eq!("カレー", worker.token(2).surface());
+/// assert_eq!("の", worker.token(3).surface());
+/// assert_eq!("街", worker.token(4).surface());
+/// assert_eq!("神保町", worker.token(5).surface());
+/// assert_eq!("へ", worker.token(6).surface());
+/// assert_eq!("ようこそ", worker.token(7).surface());
+/// assert_eq!("。", worker.token(8).surface());
+/// ```
 pub fn read_dict() -> Result<vibrato::Dictionary, Box<dyn std::error::Error>> {
     let data0 = include_bytes!("../dict/mecab-ipadic-neologd.dic.zst.aa").as_slice();
     let data1 = include_bytes!("../dict/mecab-ipadic-neologd.dic.zst.ab").as_slice();
